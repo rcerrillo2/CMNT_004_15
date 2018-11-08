40b183c52b3a1b3c8ae1d5cbbbd69cec0d97a2d9
Naferreyra <nferreyra@visiotech.es>
2018-11-08 12:04:55 +0100

    [UPD] 'partner_risk_insurance': Migración del módulo

--- project-addons/partner_risk_insurance/models/res_partner.py
+++ project-addons/partner_risk_insurance/models/res_partner.py
@@ -1,5 +1,3 @@
-
-# -*- encoding: utf-8 -*-
 ##############################################################################
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -17,59 +15,45 @@
 #
 ##############################################################################
 
-
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.one
+    @api.multi
     @api.depends('company_credit_limit', 'insurance_credit_limit')
     def _credit_limit(self):
-        self.credit_limit = (self.company_credit_limit +
-                             self.insurance_credit_limit)
-    @api.one
+        for partner in self:
+            partner.credit_limit = (partner.company_credit_limit + partner.insurance_credit_limit)
+
+    @api.multi
     def _is_accounting(self):
+        for partner in self:
+            accountant = self.env.ref('account.group_account_manager')
+            is_accountant = self.env.user.id in accountant.users.ids
+            if is_accountant:
+                partner.is_accounting = True
+            else:
+                partner.is_accounting = False
 
-        accountant = self.env.ref('account.group_account_manager')
-
-        is_accountant = self.env.user.id in accountant.users.ids
-
-        if is_accountant:
-            self.is_accounting = True
-        else:
-            self.is_accounting = False
-
-    credit_limit = fields.Float('Credit Limit', store=True,
-                                compute=_credit_limit)
-    company_credit_limit = fields.Float("Company's Credit Limit",
-                                        help='Credit limit granted by the '
-                                        'company.')
+    credit_limit = fields.Float("Credit Limit", store=True, compute='_credit_limit')
+    company_credit_limit = fields.Float("Company's Credit Limit", help="Credit limit granted by the company.")
     insurance_credit_limit = fields.Float("Insurance's Credit Limit",
-                                          help='Credit limit granted by the '
-                                          'insurance company.')
-    risk_insurance_coverage_percent = fields.Float(
-        "Insurance's Credit Coverage", help='Percentage of the credit covered '
-        'by the insurance.')
-    risk_insurance_requested = fields.Boolean(
-        'Insurance Requested', help='Mark this field if an insurance was '
-        'requested for the credit of this partner.')
-    risk_insurance_grant_date = fields.Date('Insurance Grant Date',
-                                            help='Date when the insurance was '
-                                            'granted by the insurance company.'
-                                            )
-    risk_insurance_code = fields.Char('Insurance Code',
-                                      help='Code assigned to this partner by '
-                                      'the risk insurance company.')
-    risk_insurance_code_2 = fields.Char('Insurance Code 2',
-                                        help='Secondary code assigned to this '
-                                        'partner by the risk insurance '
-                                        'company.')
-
-    risk_insurance_comment = fields.Text('Comments')
-
-    is_accounting = fields.Boolean('Is Acounting', compute="_is_accounting")
+                                          help="Credit limit granted by the insurance company.")
+    risk_insurance_coverage_percent = fields.Float("Insurance's Credit Coverage",
+                                                   help="Percentage of the credit covered by the insurance.")
+    risk_insurance_requested = fields.Boolean("Insurance Requested",
+                                              help="Mark this field if an insurance was requested for the credit "
+                                                   "of this partner.")
+    risk_insurance_grant_date = fields.Date("Insurance Grant Date",
+                                            help="Date when the insurance was granted by the insurance company.")
+    risk_insurance_code = fields.Char("Insurance Code",
+                                      help="Code assigned to this partner by the risk insurance company.")
+    risk_insurance_code_2 = fields.Char("Insurance Code 2",
+                                        help="Secondary code assigned to this partner by the risk insurance company.")
+    risk_insurance_comment = fields.Text("Comments")
+    is_accounting = fields.Boolean("Is Acounting", compute='_is_accounting')
 
 
 
