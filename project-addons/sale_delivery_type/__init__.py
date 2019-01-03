06260f868501dd1f30b955b1eed97969a67c77dd
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2019-01-03 11:27:10 +0100

    [MIG]'sale_delivery_type': Reorganización de directorios

--- project-addons/sale_delivery_type/__init__.py
+++ project-addons/sale_delivery_type/__init__.py
@@ -18,5 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import sale
-from . import claim
+from . import models
